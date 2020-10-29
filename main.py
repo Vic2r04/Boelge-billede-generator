88de06092329f9db561fda6b9cc610ae9fb220b8
Vic2r04 <60873899+Vic2r04@users.noreply.github.com>
2020-10-29 13:06:20 +0100

    version 2
    bedre på alle måder

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import time
 import math
 from PIL import Image
 import glob
+import time
 
 
 image = Image.open(glob.glob("./billede/*")[0])
 
 rgb = image.convert("RGB")
 
+forstørrer = 2
 
-canvas_width = image.size[0]
-canvas_height = image.size[1]
+thiccness = 4
 
-background_color = pygame.Color(0, 0, 0, 0)
+canvas_width = image.size[0]*forstørrer
+canvas_height = image.size[1]*forstørrer
+
+background_color = pygame.Color(10, 10, 10, 0)
 
 mellemrum = -1
-amplitude = 5 
+amplitude = 10
 
 pygame.init()
 
@@ -37,22 +41,31 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                pygame.image.save(screen, "bølge billede.png")
+
+
 
     surface.fill(background_color)
-
+    tid = time.time()
     for i in range(int(canvas_height/(mellemrum+amplitude))):
         frekvens = 20
         hastighed = 0
         for x in range(0, canvas_width):
             y = int((amplitude*i+(i*(mellemrum+1))) + amplitude*math.sin(frekvens*((float(x)/canvas_width)*(2*math.pi) + (hastighed*time.time()))))
             try:
-                pixelFarve = rgb.getpixel((x, y))
+                pixelFarve = rgb.getpixel((x/forstørrer, y/forstørrer))
             except:
                 pass
+            
             surface.set_at((x, y), pygame.Color(pixelFarve[0], pixelFarve[1], pixelFarve[2], 0))
+            surface.set_at((x, y+1), pygame.Color(pixelFarve[0], pixelFarve[1], pixelFarve[2], 0))
+            surface.set_at((x, y-1), pygame.Color(pixelFarve[0], pixelFarve[1], pixelFarve[2], 0))
+            surface.set_at((x, y+2), pygame.Color(pixelFarve[0], pixelFarve[1], pixelFarve[2], 0))
+            surface.set_at((x, y-2), pygame.Color(pixelFarve[0], pixelFarve[1], pixelFarve[2], 0))
+    print('frame tog:', time.time()-tid)    
 
-    # Put the surface we draw on, onto the screen
     screen.blit(surface, (0, 0))
 
-    # Show it.
     pygame.display.flip()
